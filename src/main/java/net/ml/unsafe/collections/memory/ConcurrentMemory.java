13b20ec6ec5dce60e64337bb9278d640b7c48c25
mike <michaellaucella@gmail.com>
2019-02-10 18:09:55 -0500

    removed serializable constraint on type parameter of concurrent memory

--- src/main/java/net/ml/unsafe/collections/memory/ConcurrentMemory.java
+++ src/main/java/net/ml/unsafe/collections/memory/ConcurrentMemory.java
@@ -1,10 +1,8 @@
 package net.ml.unsafe.collections.memory;
 
-import java.io.Serializable;
-
 /**
  * Access memory Concurrently
  *
  * @param <T> the type of object to manage the memory of
  */
-public interface ConcurrentMemory<T extends Serializable> extends Memory<T> { }
+public interface ConcurrentMemory<T> extends Memory<T> { }
